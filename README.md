1e3159a6459e7a2869fbd3a2d058d15ef1757022
Jack Baron <jackmbaron@gmail.com>
2024-03-11 01:52:47 +0000

    update install script

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ## Installation
 
 ```sh
-$ curl https://raw.githubusercontent.com/luludotdev/backup/master/install.sh | sh
+curl -fsSL https://raw.githubusercontent.com/luludotdev/backup/master/install.sh | sh
 ```
--- install.sh
+++ install.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 
+install_dir="${BACKUP_INSTALL_DIR:-/usr/local/bin}"
+script_name="${BACKUP_SCRIPT_NAME:-backup}"
+
+# Install Deno
 if command -v deno >/dev/null; then
   echo "" > /dev/null
 else
   curl -fsSL https://deno.land/install.sh | DENO_INSTALL="/usr/local" sh
 fi
 
-wget -O /usr/local/bin/backup https://raw.githubusercontent.com/luludotdev/backup/master/backup.ts
-chmod +x /usr/local/bin/backup
+# Install borg
+if command -v borg >/dev/null; then
+  echo "" > /dev/null
+else
+  # TODO: Install borg
+  echo "" > /dev/null
+fi
+
+# Install script
+script_path="$install_dir/$script_name"
+wget -q -O $script_path https://raw.githubusercontent.com/luludotdev/backup/master/backup.ts
+chmod +x $script_path
