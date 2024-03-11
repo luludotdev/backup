62dcefa299b77600e5cb99372dd495f5ac6fbc95
Jack Baron <jackmbaron@gmail.com>
2024-03-11 01:36:29 +0000

    installation script

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ else
   curl -fsSL https://deno.land/install.sh | DENO_INSTALL="/usr/local" sh
 fi
 
-# wget -O /usr/local/bin/backup
+wget -O /usr/local/bin/backup https://raw.githubusercontent.com/luludotdev/backup/master/backup.ts
 chmod +x /usr/local/bin/backup
