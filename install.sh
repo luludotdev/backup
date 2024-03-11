93445cd2ef7a9c41697fff2535a44a084ec1f5d0
Jack Baron <jackmbaron@gmail.com>
2024-03-11 03:21:51 +0000

    only emit borg info if not installed

--- install.sh
+++ install.sh
@@ -3,26 +3,25 @@
 install_dir="${BACKUP_INSTALL_DIR:-/usr/local/bin}"
 script_name="${BACKUP_SCRIPT_NAME:-backup}"
 
-# Install Deno
+# deno
 if command -v deno >/dev/null; then
   echo "" > /dev/null
 else
   curl -fsSL https://deno.land/install.sh | DENO_INSTALL="/usr/local" sh
 fi
 
-# Install borg
-if command -v borg >/dev/null; then
-  echo "" > /dev/null
-else
-  # TODO: Install borg
-  echo "" > /dev/null
-fi
-
-# Install script
+# script
 script_path="$install_dir/$script_name"
 wget -q -O $script_path https://raw.githubusercontent.com/luludotdev/backup/master/backup.ts
 chmod +x $script_path
 
 echo "backup script installed to $script_path"
-echo "make sure to install borg"
-echo "https://borgbackup.readthedocs.io/en/stable/installation.html"
+
+# borg
+if command -v borg >/dev/null; then
+  echo "" > /dev/null
+else
+  # TODO: Install borg automatically
+  echo "make sure to install borg"
+  echo "https://borgbackup.readthedocs.io/en/stable/installation.html"
+fi
